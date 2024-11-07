aa00080fda4c6489b797874d606ee13245d93803
huoxj <2567499168@qq.com>
2024-11-07 21:46:05 +0800

    initial commit

---
+++ bgdata_backend/src/main/java/nju/nailong/bgdata_backend/BgdataBackendApplication.java
@@ -0,0 +1,13 @@
+package nju.nailong.bgdata_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgdataBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgdataBackendApplication.class, args);
+    }
+
+}
---
+++ bgdata_backend/src/test/java/nju/nailong/bgdata_backend/BgdataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nju.nailong.bgdata_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgdataBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
