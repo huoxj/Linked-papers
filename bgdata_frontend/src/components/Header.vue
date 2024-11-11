52ee2535c85aa575f63ae8c58f503c05e42aea94
huoxj <2567499168@qq.com>
2024-11-11 21:51:28 +0800

    view: home view outline

---
+++ bgdata_frontend/src/components/Header.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header_wrapper">
+    <v-container fluid>
+      <v-row>
+        <v-col md="1"></v-col>
+        <v-col md="2" class="col_center">
+          <router-link to="/" class="no_link">
+            <p class="h2 theme-white">Linked Papers</p>
+          </router-link>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col md="2" class="col_center" style="justify-content: right">
+          <router-link to="/login">
+            <p class="h3 theme-white underline">Log in</p>
+          </router-link>
+        </v-col>
+        <v-col md="2" class="col_center">
+          <router-link to="/register">
+            <p class="h3 theme-white underline">Register for FREE</p>
+          </router-link>
+        </v-col>
+        <v-col md="1"></v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+.header_wrapper{
+  background-color: #C12127;
+  width: 100%;
+  height: 70px;
+}
+.no_link{
+  text-decoration: none;
+}
+.underline{
+  text-decoration: underline;
+}
+.col_center{
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ bgdata_frontend/src/views/Home.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <div class="upper-fill">
+    <v-container class="search-wrapper">
+      <v-row style="height: 15vh"></v-row>
+      <v-row>
+        <p class="h1 theme-dark">Explore Linked Papers</p>
+      </v-row>
+      <v-row>
+        <v-icon icon="mdi-book-open-blank-variant" size="x-large" class="theme-dark"></v-icon>
+        <p class="h2 theme-gray">&nbsp Big data powered paper search engine</p>
+      </v-row>
+      <v-row><br><br></v-row>
+      <v-row>
+        <v-col md="9" >
+          <v-text-field
+            placeholder="Search by title or abstract"
+            single-line
+            variant="solo"
+          ></v-text-field>
+        </v-col>
+        <v-col md="2">
+          <v-btn class="search-button" size="x-large">Search</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+.upper-fill {
+  background-color: #f1f1f1;
+  width: 100%;
+  height: 45vh;
+}
+.search-wrapper {
+  width: 140vh;
+  height: 30vh;
+}
+.search-button {
+  background-color: #C12127;
+  color: white;
+}
+</style>
