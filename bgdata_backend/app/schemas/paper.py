a15e5525513718c83eb9595ce1134d15753af958
Fradow <cirnofantasy@gmail.com>
2024-11-28 09:42:41 +0800

    Fix: create self-referential relationship for reference field in PaperInDB

--- bgdata_backend/app/schemas/paper.py
+++ bgdata_backend/app/schemas/paper.py
@@ -1,10 +1,18 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
 
 class PaperInDB(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
   abstract: str = Field(index=True)
   title: str = Field(index=True)
-  references: list[int] = Field(index=True)
   year: str = Field(index=True)
   category: str = Field(index=True)
+
+  references: list['PaperInDB'] = Relationship(
+    back_populates='referenced_by',
+  )
+  referenced_by: list['PaperInDB'] = Relationship(
+    back_populates='references',
+    sa_relationship_kwargs={'remote_side': 'PaperInDB.id'}
+  )
+  reference_id: int | None = Field(default=None, foreign_key='paperindb.id')
