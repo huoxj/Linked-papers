559475a07e8de78d9c25bbe530ac260dd3690954
xinxiang <221850120@smail.nju.edu.cn>
2024-11-29 19:24:37 +0800

    frontend: 修复了user.ts中请求的bug

--- bgdata_frontend/src/api/user.ts
+++ bgdata_frontend/src/api/user.ts
@@ -2,10 +2,13 @@ import {post} from "@/utils/request";
 import {USER_MODULE} from "@/api/_prefix";
 import type {UserInfo} from "@/utils/types";
 
-export const reqLogin = (loginInfo: UserInfo) => {
-  return post<UserInfo>(`${USER_MODULE}/login`, loginInfo);
+export const reqLogin = (email: string, password: string) => {
+    return post<UserInfo>(`${USER_MODULE}/login`, {email, password});
 }
 
-export const reqRegister = (registerInfo: UserInfo) => {
-  return post<UserInfo>(`${USER_MODULE}/register`, registerInfo);
+// export const reqRegister = (registerInfo: UserInfo) => {
+//   return post<UserInfo>(`${USER_MODULE}/register`, registerInfo);
+// }
+export const reqRegister = (email: string, username: string, password: string, premium: boolean) => {
+    return post<UserInfo>(`${USER_MODULE}/register`, {email, username, password, premium});
 }
